e18362d02b375f5ba0f4312634df252a005e45c0
unknown <bryanricardo.florencioabreu@gmail.com>
2021-08-31 16:08:41 +0200

    fix ortography errors

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,9 +5,9 @@ const About = () => {
     return (
         <div className="about-container">
             <div className="about-desc">
-                <h3>let me tell you something about me!</h3>
+                <h3>Let me tell you something about me!</h3>
                 <p>I am a very passionate person, I love to challenge myself to improve in every aspect of my life.
-                 I learned how to code from myself and I really love it. Now, I look for a way to show the world my passion and help others.</p>
+                 I learned how to code by myself and I really love it. Now, I look for a way to show the world my passion and help others.</p>
             </div>
             <div className="about-img">
                 <img src={foto} alt="about" />
